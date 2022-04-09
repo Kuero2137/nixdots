4b25555e27fe4c8b859e5fcf3972ea754e206512
Kuero <kuero@protonmail.ch>
2022-04-09 09:28:16 +0200

    Cleanup

---
+++ home/others/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, ... }:
+
+{
+  programs.home-manager.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    documents = "$HOME/stuff/other/";
+    download = "$HOME/stuff/downloads/";
+    videos = "$HOME/stuff/other/";
+    music = "$HOME/stuff/other/";
+    pictures = "$HOME/stuff/pics/";
+    desktop = "$HOME/stuff/desktop/";
+    publicShare = "$HOME/stuff/other/";
+    templates = "$HOME/stuff/other/";
+  };
+
+  programs.gpg = {
+    enable = true;
+  };
+  home.stateVersion = "21.11";
+}
