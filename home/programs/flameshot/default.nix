8d42891dc4051e734692ba224b74f097ef94267e
Kuero <kuero@protonmail.ch>
2022-04-11 21:53:01 +0200

    Added other programs

---
+++ home/programs/flameshot/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, config, theme, ... }:
+
+{
+  services.flameshot = with theme.colors; {
+    enable = true;
+    settings = {
+      General = {
+        showStartupLaunchMessage = false;
+        uiColor = "#${ac}";
+        contrastUiColor = "#${fg}";
+        drawColor = "#${c1}";
+        showHelp = false;
+        showSidePanelButton = false;
+      };
+    };
+  };
+}
