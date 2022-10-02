331c99959badecb37c7ce6132e46da5931415b40
Kuero2137 <kuero@protonmail.ch>
2022-10-02 14:05:59 +0200

    some packages added

--- home/kuero/pkgs.nix
+++ home/kuero/pkgs.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     tdesktop discord htop libreoffice yt-dlp p7zip
     ookla-speedtest filezilla firefox neofetch
-    ffmpeg gitkraken qbittorrent 
+    ffmpeg gitkraken qbittorrent dwarfs fuse-overlayfs
     #gaming
     wine-staging lutris grapejuice
   ];
