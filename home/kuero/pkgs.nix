62b780ada09e5a960e7d94691d53eed658ae07ce
Kuero2137 <kuero@protonmail.ch>
2022-10-15 10:09:58 +0200

    +pkg

--- home/kuero/pkgs.nix
+++ home/kuero/pkgs.nix
@@ -5,7 +5,7 @@
     tdesktop discord htop libreoffice yt-dlp p7zip
     ookla-speedtest filezilla firefox neofetch
     ffmpeg gitkraken qbittorrent dwarfs fuse-overlayfs
-    nicotine-plus niv slack mongodb-compass
+    nicotine-plus niv slack mongodb-compass sublime4-dev
     #gaming
     wine-staging lutris grapejuice
     #others
